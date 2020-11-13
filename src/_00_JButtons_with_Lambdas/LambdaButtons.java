88caf08c834afed5ea931f7cf006e76186ac7b7f
root <root@6747f270c8d8>
2020-11-13 02:26:37 +0000

    started lambdas

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,12 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -21,6 +24,22 @@ public class LambdaButtons {
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
 		
+		addNumbers.addActionListener((ActionEvent e)->{
+			String numOne=JOptionPane.showInputDialog("Type a number.");
+			int yay=Integer.parseInt(numOne);
+			String numTwo=JOptionPane.showInputDialog("Type a number.");
+			int unicorn=Integer.parseInt(numTwo);
+			System.out.println("The sum is " + (yay+unicorn));
+		});
+		randNumber.addActionListener((ActionEvent e)->{
+			Random random=new Random();
+			int r=random.nextInt();
+			System.out.println(r);
+		});
+		tellAJoke.addActionListener((ActionEvent e)->{
+			JOptionPane.showMessageDialog(null, "Hear about the new restarant called Karma?");
+			JOptionPane.showMessageDialog(null,"There is no menu: You get what you deserve.");          
+		});
 		window.setVisible(true);
 		window.pack();
 	}
