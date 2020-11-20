17a96e01c17d8e33fe3a8c26a3ede20183fb4bba
root <root@bee8407b652b>
2020-11-20 02:25:02 +0000

    i

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -6,24 +6,45 @@ public class LambdaMethods {
 	
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
-		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
-				System.out.println(s);
-			}
-		}, "repeat");
+		//printCustomMessage((s)->{
+			//for(int i = 0; i < 10; i++) {
+				//System.out.println(s);
+			//}
+		//}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		printCustomMessage((s)->{
-			for (int i = 0; i < s.length(); i--) {
-				System.out.println(s.ge);
-			}
-		}, "backwards");
+		//printCustomMessage((s)->{
+			//for (int i = s.length()-1; i > s.length(); i--) {
+				//System.out.println(s.charAt(i));
+			//}
+		//}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			for (int i = 0; i <s.length(); i++) {
+				if(i%2==0) {
+					System.out.println(Character.toUpperCase(s.charAt(i)));
+				}else {
+					System.out.println(Character.toLowerCase(s.charAt(i)));
+				}
+			}
+		}, "Sizes");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		//printCustomMessage((s)->{
+			//for (int i = 0; i < s.length(); i++) {
+				//System.out.print(s.charAt(i) + " .");
+			//}
+		//}, "Stringy");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+	     //printCustomMessage((s)->{
+	    	 //for (int i = 0; i < s.length(); i++) {
+				 //if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u') {
+	    		 
+	    	 //}else {
+	    		 //S//ystem.out.println(s.charAt(i));
+	    	 //}
+			//}
+	    	
+	    // }, "Vowels");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
